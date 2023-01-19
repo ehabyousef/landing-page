00546a31667d2d7674cab2d7130507b39a1bcf7f
‪Ehab Yousef‬‏ <74505597+ehabyousef@users.noreply.github.com>
2023-01-19 22:06:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# My First Project is Landing Page
+
+## Table of Contents
+
+- [Instructions](#instructions)
+
+## Instructions
+
+This Project about landing page have 4 sections and navgation bar to scroll between sections with smoothy way
+and every section have active class with an animation
+
+## Technologies
+
+I used js to convert the project from static to daynamic and interactive version by using javascript
+
+## References
+
+I searched some websites for some information like Google , youtube , MDN , W3shools , searched about some function and method to help me in the project
---
+++ js/app.js
@@ -0,0 +1,57 @@
+//Start Global Variables
+
+const sections = document.querySelectorAll("section");
+const navigationBar = document.getElementById("navbar__list");
+
+//End Global Variables
+
+// Start Creating Navigation Bar sections
+function createnavlist() {
+  //access every section in html
+  for (section of sections) {
+    //caught section name and id
+    secName = section.getAttribute("data-nav");
+    secId = section.getAttribute("id");
+
+    //create header element
+
+    listcontent = document.createElement("li");
+
+    listcontent.innerHTML = `<a class='menu__link' data-nav="${section.id}" href='#${secId}'>${section.dataset.nav}</a>`;
+    //add item to navbar list
+
+    navigationBar.appendChild(listcontent);
+  }
+}
+// End Navigation Bar section
+createnavlist();
+
+// start making activate section
+window.onscroll = function () {
+  document.querySelectorAll("section").forEach((activate) => {
+    let activeLink = navigationBar.querySelector(`[data-nav=${activate.id}]`);
+    if (
+      activate.getBoundingClientRect().top >= -400 &&
+      activate.getBoundingClientRect().top <= 150
+    ) {
+      //activate this section
+      activate.classList.add("your-active-class");
+      activeLink.classList.add("active-link");
+    } else {
+      // sleep the section we don't  use
+      activate.classList.remove("your-active-class");
+      activeLink.classList.remove("active-link");
+    }
+  });
+};
+
+// making the scroll behavior smooth
+navigationBar.addEventListener("click", (e) => {
+  e.preventDefault();
+  const srl = e.target;
+  if (srl.classList.contains("menu__link")) {
+    const id = srl.getAttribute("href").slice(1);
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+//media query
